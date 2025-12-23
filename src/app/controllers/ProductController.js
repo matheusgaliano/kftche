ee7183af8fc531839cf15116b7ca472fc7f83e23
matheusgaliano <galiano.matheus@outlook.com>
2025-12-23 19:51:13 -0300

    feat: add ProductController and define product creation route

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,21 @@
+import * as Yup from "yup";
+
+class ProductController {
+  async store(request, response) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      price: Yup.number().required(),
+      catgory: Yup.string().required(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false, strict: true });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    return response.status(201).json({ message: "Product created!" });
+  }
+}
+
+export default new ProductController();
