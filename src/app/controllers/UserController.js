72c542a83a9066c143a7d44960164ef210707db7
matheusgaliano <galiano.matheus@outlook.com>
2025-12-23 09:10:54 -0300

    feat: add validation schema for user registration and improve error handling

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,22 @@
 import User from "../models/User.js";
 import { v4 } from "uuid";
+import * as Yup from "yup";
 
 class UserController {
   async store(request, response) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().min(6).required(),
+      admin: Yup.boolean().required(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false, strict: true });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
     const { name, email, password_hash, admin } = request.body;
 
     const existingUser = await User.findOne({
@@ -12,9 +26,7 @@ class UserController {
     });
 
     if (existingUser) {
-      return response
-        .status(400)
-        .json({ error: "Este e-mail já está cadastrado!" });
+      return response.status(400).json({ error: "Email already taken!" });
     }
 
     const user = await User.create({
