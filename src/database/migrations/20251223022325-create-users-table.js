1a6fefeb86c4a2929908ffe428f21deeb349a7f2
matheusgaliano <galiano.matheus@outlook.com>
2025-12-22 23:47:39 -0300

    feat: create migration for users table

---
+++ src/database/migrations/20251223022325-create-users-table.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
