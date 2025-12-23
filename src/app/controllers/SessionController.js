34105348d343d7efd82b0f58f508f038af2267d4
matheusgaliano <galiano.matheus@outlook.com>
2025-12-23 10:01:00 -0300

    feat: implement user authentication logic in SessionController

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,56 @@
+import * as Yup from "yup";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
 class SessionController {
   async store(request, response) {
-    return response.status(200).json({ ok: true });
+    const schema = Yup.object({
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+    });
+
+    const isValid = await schema.isValid(request.body, {
+      abortEarly: false,
+      strict: true,
+    });
+
+    const emailOrPasswordIncorrect = () => {
+      return response
+        .status(400)
+        .json({ error: "Email or password incorrect!" });
+    };
+
+    if (!isValid) {
+      emailOrPasswordIncorrect();
+    }
+
+    const { email, password } = request.body;
+
+    const existingUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!existingUser) {
+      emailOrPasswordIncorrect();
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password_hash
+    );
+
+    if (!isPasswordCorrect) {
+      emailOrPasswordIncorrect();
+    }
+
+    return response.status(200).json({
+      id: existingUser.id,
+      name: existingUser.name,
+      email: existingUser.email,
+      admin: existingUser.admin,
+    });
   }
 }
 
