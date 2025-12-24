00399a1d8c750de676024bfa399106615fa2cd55
matheusgaliano <galiano.matheus@outlook.com>
2025-12-24 00:24:21 -0300

    feat: implement JWT authentication in session controller and add auth configuration

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,8 @@
 import * as Yup from "yup";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import authConfig from "../../config/auth.js";
 
 class SessionController {
   async store(request, response) {
@@ -45,11 +47,16 @@ class SessionController {
       emailOrPasswordIncorrect();
     }
 
+    const token = jwt.sign({ id: existingUser.id }, authConfig.secret, {
+      expiresIn: authConfig.expiresIn,
+    });
+
     return response.status(200).json({
       id: existingUser.id,
       name: existingUser.name,
       email: existingUser.email,
       admin: existingUser.admin,
+      token,
     });
   }
 }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "afebf0c3ea7a87b0a259271fdab800b9",
+  expiresIn: "7d",
+};
