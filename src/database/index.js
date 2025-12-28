4a2ba2bbf27bc610354c1e61d7c4a29858dee049
matheusgaliano <galiano.matheus@outlook.com>
2025-12-28 07:57:59 -0300

    configuring mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize";
+import mongoose from "mongoose";
 import databaseConfig from "../config/database.cjs";
 import User from "../app/models/User.js";
 import Product from "../app/models/Product.js";
@@ -9,6 +10,7 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(databaseConfig);
@@ -18,6 +20,9 @@ class Database {
         (model) => model.associate && model.associate(this.connection.models)
       );
   }
-}
 
+  mongo() {
+    this.mongoConnection = mongoose.connect("mongodb://localhost:27017/kftche");
+  }
+}
 export default new Database();
