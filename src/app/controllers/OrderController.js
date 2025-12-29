d4ac234a0ea63ee5aedca9d4235fec897f8ef8a0
matheusgaliano <galiano.matheus@outlook.com>
2025-12-29 08:06:17 -0300

    Saving requests in MongoDB

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,6 +1,7 @@
 import * as Yup from "yup";
 import Product from "../models/Product.js";
 import Category from "../models/Category.js";
+import Order from "../schemas/Order.js";
 
 class OrderController {
   async store(request, response) {
@@ -59,7 +60,9 @@ class OrderController {
       status: "Pedido realizado!",
     };
 
-    return response.status(201).json(order);
+    const newOrder = await Order.create(order);
+
+    return response.status(201).json(newOrder);
   }
 }
 
