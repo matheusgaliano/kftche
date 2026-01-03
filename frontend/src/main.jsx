adcf78788655c2d291c588db75756df6cde54504
matheusgaliano <galiano.matheus@outlook.com>
2026-01-03 12:15:38 -0300

    chore(frontend): setup styled-components and fix folder structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import globalStyles from "./styles/globalStyles";
+import { Login } from "./containers/Login"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Login />
+    <globalStyles/>
   </React.StrictMode>
 );
