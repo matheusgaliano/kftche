b0c9fab7ad1694d770373f6424f6e18a8592cd91
matheusgaliano <galiano.matheus@outlook.com>
2026-01-03 23:39:27 -0300

    feat(login): init login screen structure and assets

--- frontend/src/containers/Login/index.jsx
+++ frontend/src/containers/Login/index.jsx
@@ -1,7 +1,33 @@
+import { Button, Container, Form, InputContainer, LeftContainer, Link, RightContainer, Title } from "./styles";
+import Logo from "../../assets/logo-kftche1.svg"
+
 export function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo1-kftche"/>
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>KFTchê</span>! Acesse com seu <span>Login e Senha</span>.
+        </Title>
+        <Form>
+          <InputContainer>
+          <label>E-mail</label>
+          <input type="email"/>
+          </InputContainer>
+
+          <InputContainer>
+          <label>Senha</label>
+          <input type="password"/>
+          </InputContainer>
+
+          <Link>Esqueci minha senha.</Link>
+          <Button>Entrar</Button>
+        </Form>
+        <Link>Não possui conta? Clique aqui!</Link>
+      </RightContainer>
+
+    </Container>
   );
 }
