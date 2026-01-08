28fc53cc7ddb17d69cb0c4f664d5e779a76c6c64
matheusgaliano <galiano.matheus@outlook.com>
2026-01-08 07:10:36 -0300

    feat: fix button validation login

--- frontend/src/containers/Login/index.jsx
+++ frontend/src/containers/Login/index.jsx
@@ -1,9 +1,31 @@
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 import { Container, Form, InputContainer, LeftContainer, RightContainer, Title } from "./styles";
 import Logo from "../../assets/logo-kftche1.svg"
 
 import { Button } from '../../components/Button'
 
 export function Login() {
+
+  const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().min(6).required(),
+  })
+  .required()
+
+
+    const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = (data) => console.log(data)
+
+
   return (
     <Container>
       <LeftContainer>
@@ -15,18 +37,18 @@ export function Login() {
           <br />
           Acesse com seu <span>Login e Senha.</span>
         </Title>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <InputContainer>
           <label>E-mail</label>
-          <input type="email"/>
+          <input type="email" {...register("email")}/>
           </InputContainer>
 
           <InputContainer>
           <label>Senha</label>
-          <input type="password"/>
+          <input type="password" {...register("password")}/>
           </InputContainer>
 
-          <Button>Entrar</Button>
+          <Button type='submit'>Entrar</Button>
         </Form>
         <p>Não possui conta? <a>Clique aqui!</a></p>
       </RightContainer>
