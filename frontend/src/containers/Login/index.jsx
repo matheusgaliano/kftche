05ab14926328e2905a8bfb68af31f13df42e99de
matheusgaliano <galiano.matheus@outlook.com>
2026-01-08 07:21:39 -0300

    feat: validation form login

--- frontend/src/containers/Login/index.jsx
+++ frontend/src/containers/Login/index.jsx
@@ -10,8 +10,8 @@ export function Login() {
 
   const schema = yup
   .object({
-    email: yup.string().email().required(),
-    password: yup.string().min(6).required(),
+    email: yup.string().email('Digite um e-mail válido!').required('O e-mail é obrigatório.'),
+    password: yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres!').required('Digite uma senha!'),
   })
   .required()
 
@@ -41,11 +41,13 @@ export function Login() {
           <InputContainer>
           <label>E-mail</label>
           <input type="email" {...register("email")}/>
+          <p>{errors?.email?.message}</p>
           </InputContainer>
 
           <InputContainer>
           <label>Senha</label>
           <input type="password" {...register("password")}/>
+          <p>{errors?.password?.message}</p>
           </InputContainer>
 
           <Button type='submit'>Entrar</Button>
