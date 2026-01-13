d42008d8a19933d7305cbdf1a71970dda19f907a
matheusgaliano <galiano.matheus@outlook.com>
2026-01-13 19:35:18 -0300

    style: add home banner image and container styles

--- frontend/src/containers/Home/index.jsx
+++ frontend/src/containers/Home/index.jsx
@@ -1,8 +1,22 @@
-
+import { Banner, Container, Content } from "./styles";
 
 
 export function Home(){
     return (
-        <div>Home</div>
+        <main>
+            <Banner>
+            <h1>Bem-Vindo(a)!</h1>
+            </Banner>
+            <Container>
+                <Content>
+                    <div>
+                        Carrossel de Categoria
+                    </div>
+                    <div>
+                        Carrossel de Produtos
+                    </div>
+                </Content>
+            </Container>
+        </main>
     )
 }
---
+++ frontend/src/containers/Home/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import BannerHome from '../../assets/banner-home.svg'
+import Background from '../../assets/background.svg'
+
+export const Banner = styled.div`
+    background: url('${BannerHome}');
+    background-size: cover;
+    background-position: center;
+    height: 460px;
+
+    h1{
+        font-family: 'Road Rage', sans-serif;
+        font-size: 80px;
+        color: #FFFFFF;
+        position: absolute;
+        right: 20%;
+        top: 10%;
+
+    }
+`
+
+export const Container = styled.section`
+    background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('${Background}');
+    height: 500px;
+
+`
+
+export const Content = styled.div``
