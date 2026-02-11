b13fbc58314108473a767bcd4fc5883f5fea9549
matheusgaliano <galiano.matheus@outlook.com>
2026-02-10 23:35:24 -0300

    feat: creation menu

---
+++ frontend/src/containers/Menu/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Banner, CategoryMenu, ProductsContainer } from "./styles";
+
+export function Menu(){
+    return (
+        <Container>
+            <Banner>
+            <h1>O MELHOR
+                <br/>
+                HAMBURGUER
+                <br/>
+                ESTÁ AQUI!!!
+            </h1>
+            <span>Este cardápio é o melhor da região!</span>
+            </Banner>
+            <CategoryMenu>
+
+            </CategoryMenu>
+
+            <ProductsContainer>
+
+
+            </ProductsContainer>
+            </Container>
+    )
+}
---
+++ frontend/src/containers/Menu/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+`
+export const Banner = styled.div`
+
+`
+export const CategoryMenu = styled.div`
+
+`
+export const ProductsContainer = styled.div`
+
+`
+
