80efc9d90c17edd8bbefa74acac33632df52bdb1
matheusgaliano <galiano.matheus@outlook.com>
2026-02-12 19:10:23 -0300

    feat: logical construction of a cart

--- frontend/src/hooks/CartContext.jsx
+++ frontend/src/hooks/CartContext.jsx
@@ -1,4 +1,4 @@
-import { useContext, createContext, useEffect, useState } from "react";
+import { useContext, createContext, useEffect, useState, use } from "react";
 
 const CartContext = createContext({});
 
@@ -6,25 +6,72 @@ export const CartProvider = ({ children }) => {
 const [cartProducts, setCartProducts] = useState([]);
 
 const putProductInCart = (product) => {
+   const cartIndex = cartProducts.findIndex(prd => prd.id === product.id);
 
+   let newProductsInCart = [];
+    if (cartIndex >= 0) {
+        newProductsInCart = cartProducts
+
+        newProductsInCart[cartIndex].quantity = 
+        newProductsInCart[cartIndex].quantity += 1;
+
+        setCartProducts(newProductsInCart);
+
+    } else {
+        product.quantity = 1;
+        newProductsInCart = [...cartProducts, product];
+        setCartProducts(newProductsInCart);
+    }
+        updateLocalStorage(newProductsInCart);
 }
 
 const clearCart = () => {
 
-
 }
 
 const deleteProductFromCart = (productId) => {
+    const newCart = cartProducts.filter( prd => prd.id !== productId);
 
+    setCartProducts(newCart);
+    updateLocalStorage(newCart);
 }
 
 const increaseProductQuantity = (productId) => {
+    const newCart = cartProducts.map( prd => {
+        return prd.id === productId ? { ...prd, quantity: prd.quantity + 1} : prd;
+    });
+    setCartProducts(newCart);
+    updateLocalStorage(newCart);
 
 }
 
 const decreaseProductQuantity = (productId) => {
+    const cartIndex = cartProducts.findIndex(prd => prd.id === productId);
 
+    if (cartProducts[cartIndex].quantity > 1) {
+        const newCart = cartProducts.map((prd) => {
+            return prd.id === productId 
+            ? { ...prd, quantity: prd.quantity - 1} 
+            : prd;
+        });
+        setCartProducts(newCart);
+        updateLocalStorage(newCart);
+    } else {
+        deleteProduct(productId);
+    }
 }
+
+    const updateLocalStorage = (products) => {
+    localStorage.setItem('kftche:cartInfo', JSON.stringify(products));
+}
+
+useEffect(() => {
+    const clientCartData = localStorage.getItem('kftche:cartInfo');
+
+    if (clientCartData) {
+        setCartProducts(JSON.parse(clientCartData));
+    }
+}, [])
 
     return ( <CartContext.Provider value={{ 
         cartProducts, 
@@ -45,4 +92,5 @@ export const useCart = () => {
     if (!context) {
         throw new Error('useCart must be used within a CartProvider');
     }
+    return context;
 }
--- frontend/src/hooks/index.jsx
+++ frontend/src/hooks/index.jsx
@@ -1,5 +1,5 @@
-import { Cart } from "../containers/Cart";
 import { UserProvider } from "./UserContext";
+import { CartProvider } from "./CartContext";
 
 const AppProvider = ({ children }) => {
     return (
