a2ae22f64a34040621d96a5ac7050890bb520d67
matheusgaliano <galiano.matheus@outlook.com>
2026-03-01 22:33:25 -0300

    fix: configuration header and footer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,23 +2,26 @@ import { Elements } from '@stripe/react-stripe-js'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyles from "./styles/globalStyles";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from './routes';
 import { ToastContainer } from "react-toastify";
-import { router } from "./routes";
 import AppProvider from "./hooks";
 import stripePromise from './config/stripeConfig';
 import { standardTheme } from './styles/themes/standard'
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TeamProvider theme={standardTheme}>
+    <ThemeProvider theme={standardTheme}>
       <AppProvider>
         <Elements stripe={stripePromise}>
-          <RouterProvider router={router} />
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
         </Elements>
         <GlobalStyles />
         <ToastContainer autoClose={2000} theme='dark' />
       </AppProvider>
-    </TeamProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,50 +1,30 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
-import { Cart, Checkout, CompletePayment, Home, Login, Menu, Register } from '../containers';
+import {
+    Cart,
+    Checkout,
+    CompletePayment,
+    Home,
+    Login,
+    Menu,
+    Register
+} from '../containers';
+import { UserLayout } from '../layout/UserLayout';
 
-export const router = createBrowserRouter([
+export function Router() {
+    return (
+        <Routes>
+            <Route path='/' element={<UserLayout />} >
+                <Route path='/' element={<Home />} />
+                <Route path='/menu' element={<Menu />} />
+                <Route path='/carrinho' element={<Cart />} />
+                <Route path='/checkout' element={<Checkout />} />
+                <Route path='/complete' element={<CompletePayment />} />
+            </Route>
+            <Route path='/login' element={<Login />} />
+            <Route path='/cadastro' element={<Register />} />
 
-    {
-        path: '/',
-        element:
 
-            (
-                <>
-                    <Header />
-                    <Home />
-                    <Footer />
-                </>
-            ),
-    },
-    {
-        path: '/login',
-        element: <Login />,
-    },
-    {
-        path: '/cadastro',
-        element: <Register />,
-    },
-    {
-        path: '/menu',
-        element: (
-            <>
-                <Header />
-                <Menu />
-            </>
-        ),
-    },
-    {
-        path: '/carrinho',
-        element: <Cart />
-    },
-    {
-        path: '/checkout',
-        element: <Checkout />
-    },
-    {
-        path: '/complete',
-        element: <CompletePayment />
-    }
-]);
+        </Routes>
+    )
+}
