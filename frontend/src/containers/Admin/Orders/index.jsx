e4d629498d9b47bc7024825b2610c3dec48bd653
matheusgaliano <galiano.matheus@outlook.com>
2026-03-04 12:05:29 -0300

    feat: table organization

--- frontend/src/containers/Admin/Orders/index.jsx
+++ frontend/src/containers/Admin/Orders/index.jsx
@@ -1,4 +1,3 @@
-
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,56 +6,55 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Row } from './row';
-
-function createData(name, calories, fat, carbs, protein, price) {
-    return {
-        name,
-        calories,
-        fat,
-        carbs,
-        protein,
-        price,
-        history: [
-            {
-                date: '2020-01-05',
-                customerId: '11091700',
-                amount: 3,
-            },
-            {
-                date: '2020-01-02',
-                customerId: 'Anonymous',
-                amount: 1,
-            },
-        ],
-    };
-}
-
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-    createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-    createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-    createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
+import { useEffect, useState } from 'react';
+import { api } from '../../../services/api';
 
 export function Orders() {
+    const [orders, setOrders] = useState([])
+    const [rows, setRows] = useState([])
+
+    useEffect(() => {
+
+        async function loadOrders() {
+            const { data } = await api.get('orders')
+
+            setOrders(data)
+        }
+        loadOrders()
+    }, []);
+
+    function createData(order) {
+        return {
+            name: order.user.name,
+            orderId: order._id,
+            date: order.createdAt,
+            status: order.status,
+            products: order.products,
+        };
+    }
+
+    useEffect(() => {
+        const newRows = orders.map((order) => createData(order))
+
+        setRows(newRows)
+
+    }, [orders])
+
     return (
         <TableContainer component={Paper}>
             <Table aria-label="collapsible table">
                 <TableHead>
                     <TableRow>
                         <TableCell />
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Pedido</TableCell>
+                        <TableCell>Cliente</TableCell>
+                        <TableCell>Data do Pedido</TableCell>
+                        <TableCell>Status</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map((row) => (
-                        <Row key={row.name} row={row} />
+                        <Row key={row._id} row={row} />
                     ))}
                 </TableBody>
             </Table>
--- frontend/src/containers/Admin/Orders/row.jsx
+++ frontend/src/containers/Admin/Orders/row.jsx
@@ -29,39 +29,38 @@ export function Row(props) {
                     </IconButton>
                 </TableCell>
                 <TableCell component="th" scope="row">
-                    {row.name}
+                    {row.orderId}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.date}</TableCell>
+                <TableCell align="right">{row.status}</TableCell>
             </TableRow>
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
                     <Collapse in={open} timeout="auto" unmountOnExit>
                         <Box sx={{ margin: 1 }}>
                             <Typography variant="h6" gutterBottom component="div">
-                                History
+                                Pedido
                             </Typography>
                             <Table size="small" aria-label="purchases">
                                 <TableHead>
                                     <TableRow>
-                                        <TableCell>Date</TableCell>
-                                        <TableCell>Customer</TableCell>
-                                        <TableCell align="right">Amount</TableCell>
-                                        <TableCell align="right">Total price ($)</TableCell>
+                                        <TableCell>Quantidade</TableCell>
+                                        <TableCell>Produto</TableCell>
+                                        <TableCell>Categoria</TableCell>
+                                        <TableCell></TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {row.history.map((historyRow) => (
-                                        <TableRow key={historyRow.date}>
+                                    {row.products.map((product) => (
+                                        <TableRow key={product.id}>
                                             <TableCell component="th" scope="row">
-                                                {historyRow.date}
+                                                {product.id}
                                             </TableCell>
-                                            <TableCell>{historyRow.customerId}</TableCell>
-                                            <TableCell align="right">{historyRow.amount}</TableCell>
-                                            <TableCell align="right">
-                                                {Math.round(historyRow.amount * row.price * 100) / 100}
+                                            <TableCell>{product.name}</TableCell>
+                                            <TableCell>{product.category}</TableCell>
+                                            <TableCell>
+                                                <img src={product.url} alt={product.name} />
                                             </TableCell>
                                         </TableRow>
                                     ))}
@@ -77,18 +76,19 @@ export function Row(props) {
 
 Row.propTypes = {
     row: PropTypes.shape({
-        calories: PropTypes.number.isRequired,
-        carbs: PropTypes.number.isRequired,
-        fat: PropTypes.number.isRequired,
-        history: PropTypes.arrayOf(
+        orderId: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        products: PropTypes.arrayOf(
             PropTypes.shape({
-                amount: PropTypes.number.isRequired,
-                customerId: PropTypes.string.isRequired,
-                date: PropTypes.string.isRequired,
+                id: PropTypes.number.isRequired,
+                category: PropTypes.string.isRequired,
+                name: PropTypes.string.isRequired,
+                price: PropTypes.number.isRequired,
+                quantity: PropTypes.number.isRequired,
+                url: PropTypes.string.isRequired,
             }),
         ).isRequired,
-        name: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-        protein: PropTypes.number.isRequired,
+        status: PropTypes.string.isRequired,
     }).isRequired,
 };
