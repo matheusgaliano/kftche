533406df4d23ecb1dda5fdb3cd28567f38b43ffe
matheusgaliano <galiano.matheus@outlook.com>
2026-03-06 15:42:04 -0300

    fix: deleted bagde

--- frontend/src/components/CartButton/styles.js
+++ frontend/src/components/CartButton/styles.js
@@ -13,3 +13,22 @@ export const ContainerButton = styled.button`
         background-color: #a71111;
     }
 `
+
+export const Badge = styled.span`
+  position: absolute;
+  top: -5px;     
+  right: -5px;    
+  
+  background-color: #ff0000;
+  color: #ffffff;
+  font-size: 12px;
+  font-weight: bold;
+  
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
